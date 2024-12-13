7a18766c26492d6826f8ab1ff117ec24808a4478
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-12-13 14:30:47 +0000

    Upload cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import bodyParser from "body-parser";
 import express from 'express';
 import { routes } from "./routes/index.js";
+import cors from 'cors'
 import i18next from './config/i18next.js';
 import i18nextMiddleware from 'i18next-express-middleware'; 
 
@@ -9,6 +10,11 @@ app.use(i18nextMiddleware.handle(i18next));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  allowedHeaders: ['Authorization', 'Content-type', 'X-Requested-With'],
+};
+app.use(cors(corsOptions));
 app.use('/api', routes);
 
 const port = 3000;
